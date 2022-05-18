caed36f8ae8fc4d8e2eacd49390d22a231d0a41a
RSIlya <_ris_@mail.ru>
2022-05-18 22:30:28 +0500

    Added solution to problem /2.1-databases/work_with_database/

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,10 +1,14 @@
 import csv
 
 from django.core.management.base import BaseCommand
+from django.utils.text import slugify
+
 from phones.models import Phone
 
 
 class Command(BaseCommand):
+    help = 'Import data from csv file to DB'
+
     def add_arguments(self, parser):
         pass
 
@@ -13,5 +17,15 @@ class Command(BaseCommand):
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            try:
+                Phone.objects.get(name=phone['name'])
+            except Phone.DoesNotExist:
+                print(f"Добавление в базу данные о {phone['name']}")
+                Phone.objects.create(
+                    name=phone['name'],
+                    image=phone['image'],
+                    price=phone['price'],
+                    release_date=phone['release_date'],
+                    lte_exists=phone['lte_exists'],
+                    slug=slugify(phone['name'])
+                )
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+
+    name = models.CharField(max_length=255, verbose_name='Наименование')
+    image = models.URLField(max_length=255, verbose_name='Изображение')
+    price = models.PositiveIntegerField(verbose_name='Цена')
+    release_date = models.DateField(verbose_name='Дата релиза')
+    lte_exists = models.BooleanField(verbose_name='Поддержка lte')
+    slug = models.SlugField(max_length=255, unique=True, verbose_name='URL')
