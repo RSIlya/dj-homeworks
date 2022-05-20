4151f3856c707ca1041c4fc1fd92109cc394b61f
RSIlya <_ris_@mail.ru>
2022-05-20 17:19:30 +0500

    Added solution to problem /2.1-databases/models_list_displaying/

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
+from books.models import Book
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    context = {'books': Book.objects.all()}
     return render(request, template, context)
+
+
+def books_on_date_view(request, pub_date):
+    template = 'books/books_list.html'
+    context = {
+        'books': Book.objects.filter(pub_date=pub_date),
+        'prev_book': Book.objects.order_by('-pub_date').exclude(pub_date__gte=pub_date).first(),
+        'next_book': Book.objects.order_by('pub_date').filter(pub_date__gt=pub_date).first(),
+    }
+    return render(request, template, context)
+
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from books.views import books_view
+from books.views import books_view, books_on_date_view
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    path('', books_view, name='index'),
+    path('books/', books_view, name='books'),
+    path('books/<pub_date>/', books_on_date_view, name='books_on_date'),
     path('admin/', admin.site.urls),
 ]
